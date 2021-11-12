6dffba59536acf80484abb05633dfe8bf91a3287
GloryJumbo <bed-com-23-17@unima.ac.mw>
2021-11-12 22:03:14 +0200

    first commit

---
+++ app/src/main/java/com/example/accommodationsystem/UserRegistrationForm.java
@@ -0,0 +1,14 @@
+package com.example.accommodationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserRegistrationForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_registration);
+    }
+}
